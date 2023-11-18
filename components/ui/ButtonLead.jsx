e0cce5da0eb64abb8c6a9dc174cc4efb09446dfe
Justin Trugman <jtrugman14@gmail.com>
2023-11-17 19:48:57 -0500

    add mobile responsiveness

--- components/ui/ButtonLead.jsx
+++ components/ui/ButtonLead.jsx
@@ -26,47 +26,46 @@ const ButtonLead = () => {
     }
   };
   return (
-    <form
-        className="justify-items-center"
-      onSubmit={handleSubmit}
-    >
-  <div className="flex items-center space-x-3">
-    <input
-      required
-      type="email"
-      value={email}
-      ref={inputRef}
-      autoComplete="email"
-      placeholder="justin@timberlake.com"
-      className="input input-bordered flex-grow placeholder:opacity-60"
-      onChange={(e) => setEmail(e.target.value)}
-    />
+<form
+  className="w-full max-w-xs md:max-w-lg flex flex-col md:flex-row items-center space-y-3 md:space-y-0 md:space-x-3 justify-center"
+  onSubmit={handleSubmit}
+>
+  <input
+    required
+    type="email"
+    value={email}
+    ref={inputRef}
+    autoComplete="email"
+    placeholder="justin@timberlake.com"
+    className="input input-bordered w-full md:w-auto md:flex-grow placeholder:opacity-60"
+    onChange={(e) => setEmail(e.target.value)}
+  />
 
-    <button
-      className="btn btn-info"
-      type="submit"
-      disabled={isDisabled}
-    >
-        Sign Up
-        {isLoading ? (
-          <span className="loading loading-spinner loading-xs"></span>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-5 h-5"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
-              clipRule="evenodd"
-            />
-          </svg>
-        )}
-      </button>
-      </div>
-    </form>
+  <button
+    className="btn btn-info"
+    type="submit"
+    disabled={isDisabled}
+  >
+      Sign Up
+      {isLoading ? (
+        <span className="loading loading-spinner loading-xs"></span>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-5 h-5"
+        >
+          <path
+            fillRule="evenodd"
+            d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+            clipRule="evenodd"
+          />
+        </svg>
+      )}
+    </button>
+  </form>
+  
   );
 };
 
