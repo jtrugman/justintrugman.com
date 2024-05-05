b0157aa1374ccb939008ee97bded4cdd4f85f046
Justin Trugman <jtrugman14@gmail.com>
2024-05-05 13:05:41 -0400

    Add education

--- components/ui/items-divider.jsx
+++ components/ui/items-divider.jsx
@@ -8,8 +8,8 @@ const ItemsDivider = () => {
             <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
                 {/* Work Experience Column */}
                 <div className="space-y-4">
-                    <div className="text-center font-medium mb-4">Work Experience</div> {/* Visible only on mobile */}
-                    {experienceInfo.WorkExperience.map((item, index) => (
+                    <div className="text-center font-medium mb-4">Work & Education</div> {/* Visible only on mobile */}
+                    {experienceInfo.WorkEdu.map((item, index) => (
                         <ExperienceCard key={index}
                             title={item.Title}
                             subtitle={item.Subtitle}
