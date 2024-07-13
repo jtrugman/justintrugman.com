ce7b25349f6151d33ff1866998e756f23228f143
Justin Trugman <jtrugman14@gmail.com>
2024-07-13 13:35:26 -0400

    Added Posthog Analytics

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Footer from '@/components/Footer'
 import { GoogleAnalytics } from '@next/third-parties/google'
-
+import { CSPostHogProvider } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,11 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Footer />
-      </body>
-      <GoogleAnalytics gaId="G-6MZEE7RY73" />
+      <CSPostHogProvider>
+        <body className={inter.className}>
+          {children}
+          <Footer />
+        </body>
+        <GoogleAnalytics gaId="G-6MZEE7RY73" />
+      </CSPostHogProvider>
     </html>
   )
 }
